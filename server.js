ac27bdf92f4d5fc5852b3cfb4058d331f62e9dcc
pritam <pritam@hacker.in>
2022-09-18 22:51:56 +0530

    PWChat v1.0.0 initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { Socket } = require("socket.io");
+const app = express();
+const http = require("http").createServer(app);
+
+/* Setting the port to 4000 if the environment variable PORT is not set. */
+const port = process.env.PORT || 4000;
+
+http.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
+/* Telling express to use the view folder as the static folder. */
+app.use(express.static(__dirname + "/view"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+//socket.io
+const users = {};
+
+/* Creating a new instance of socket.io by passing the http object. */
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  console.log("Connected Successfully...");
+
+  socket.on("new-user-joined", (cname) => {
+    console.log("New User ", cname);
+    users[socket.id] = cname;
+    socket.broadcast.emit("user-joined", cname);
+  });
+
+  socket.on("disconnect", (message) => {
+    console.log(users[socket.id], " left");
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
+  });
+});
---
+++ view/chat.js
@@ -0,0 +1,69 @@
+const socket = io();
+let cname;
+
+let textarea = document.querySelector("#messageInp");
+let msga = document.querySelector(".container");
+let subBtn = document.querySelector(".bow");
+var audio = new Audio("/ring.mp3");
+
+const scrollTB = () => {
+  msga.scrollTop = msga.scrollHeight;
+};
+
+do {
+  cname = prompt("Enter your name:");
+} while (!cname);
+cname = cname.charAt(0).toUpperCase() + cname.slice(1);
+
+socket.emit("new-user-joined", cname);
+const appendNew = (message) => {
+  const messageEle = document.createElement("div");
+  messageEle.innerHTML = `<div><b>${message}</b></div>`;
+  messageEle.classList.add("join-chat");
+  msga.append(messageEle);
+};
+socket.on("user-joined", (data) => {
+  appendNew(`${data} joined the chat...`);
+});
+socket.on("left", (data) => {
+  appendNew(`${data} left the chat...`);
+});
+
+subBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let tv = textarea.value;
+
+  sendMessage(tv);
+});
+
+function sendMessage(mess) {
+  let msg = {
+    user: cname,
+    message: mess.trim(),
+  };
+  //   append message
+  appendMsg(msg, "outgoing");
+  textarea.value = "";
+  scrollTB();
+
+  //   send to socket
+  socket.emit("message", msg);
+}
+
+function appendMsg(msg, typee) {
+  let mainDiv = document.createElement("div");
+  let type = typee;
+  mainDiv.classList.add(type, "message");
+
+  let html = ` <b>${msg.user}</b><br />&nbsp;${msg.message}</div> `;
+  mainDiv.innerHTML = html;
+  msga.appendChild(mainDiv);
+}
+
+// receive messages
+/* Listening for a message from the server. */
+socket.on("message", (msg) => {
+  appendMsg(msg, "incoming");
+  audio.play();
+  scrollTB();
+});
